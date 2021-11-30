c3bbfc81ff66c17e50e6ed3b801862b8849a1b3c
Jan Miksovsky <jan@miksovsky.com>
2021-11-29 17:02:35 -0800

    Demo

--- src/thumbnail.js
+++ src/thumbnail.js
@@ -1,8 +1,8 @@
 import sharp from "sharp";
 
-export default function thumbnail(imageBuffer, width = 200) {
+export default function thumbnail(imageBuffer) {
   if (!(imageBuffer instanceof Buffer)) {
     return undefined;
   }
-  return sharp(imageBuffer).resize({ width }).toBuffer();
+  return sharp(imageBuffer).resize({ width: 200 }).toBuffer();
 }
