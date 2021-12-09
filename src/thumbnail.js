de3403db37cf0291478c114c74c45b8a0a092852
Jan Miksovsky <jan@miksovsky.com>
2021-12-09 14:00:16 -0800

    Update images, explorable

--- src/thumbnail.js
+++ src/thumbnail.js
@@ -1,8 +1,7 @@
 import sharp from "sharp";
 
 export default function thumbnail(imageBuffer) {
-  if (!(imageBuffer instanceof Buffer)) {
-    return undefined;
-  }
-  return sharp(imageBuffer).resize({ width: 200 }).toBuffer();
+  return imageBuffer instanceof Buffer
+    ? sharp(imageBuffer).resize({ width: 200 }).toBuffer()
+    : undefined;
 }
