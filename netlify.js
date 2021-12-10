9ff25d806b879b767e1925a6a9a631b5f3af3f83
Jan Miksovsky <jan@miksovsky.com>
2021-12-09 16:54:43 -0800

    Comments

--- netlify.js
+++ netlify.js
@@ -2,6 +2,12 @@ import { ExplorableGraph } from "@explorablegraph/explorable";
 
 /**
  * Expose index.html and keys.json (no initial dot) for Netlify.
+ *
+ * Netlify won't deploy files that start with a dot like .keys.json. As a
+ * workaround, we expose a `keys.json` key that contains the same content as
+ * .keys.json. A separate rewrite rule in the _redirects file will redirect
+ * requests for .keys.json (which won't be available) to keys.json (which will
+ * be available).
  */
 export default async function netlify(variant) {
   const graph = ExplorableGraph.from(variant);
@@ -22,6 +28,7 @@ export default async function netlify(variant) {
 
     async get(key) {
       if (key === "keys.json") {
+        // Special case; see note above.
         return await this.get(".keys.json");
       }
       const value = await graph.get(key);
