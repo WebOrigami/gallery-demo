9c2d659743ae2b6d2996009965a9239470ae92a1
Jan Miksovsky <jan@miksovsky.com>
2023-05-24 15:52:08 -0700

    Some reorg

--- src/thumbnail.js
+++
@@ -1,7 +0,0 @@
-import sharp from "sharp";
-
-export default function thumbnail(imageBuffer) {
-  return imageBuffer instanceof Buffer
-    ? sharp(imageBuffer).resize({ width: 200 }).toBuffer()
-    : undefined;
-}
